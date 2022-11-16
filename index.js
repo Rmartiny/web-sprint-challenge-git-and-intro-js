5ae09ea38a7fc5f25a0ec7bb0821b2772f63d724
Rmartiny <dimambroraven@gmail.com>
2022-11-16 11:22:18 -0500

    task 7

--- index.js
+++ index.js
@@ -315,7 +315,7 @@ Use lotsOfArt to do the following:
 */
 
 function lotsOfArt(array)
-{ const bigpaint =  array.map((array) => array.paintings >= 100)
+{ const bigpaint =  array.filter((array) => array.paintings >= 100)
 
   return bigpaint.name
 }
