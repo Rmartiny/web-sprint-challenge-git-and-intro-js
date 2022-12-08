614793664863e1c335a29ceffc1537d2d7236749
Rmartiny <dimambroraven@gmail.com>
2022-12-08 14:17:02 -0500

    task 8

--- index.js
+++ index.js
@@ -342,7 +342,7 @@ Use artistInfo to do the following:
 
 function artistInfo(array,name){
  for(let i = 0; i < array.length; i++){
-  if(array[i].name ===true);{
+  if(array.name === true);{
     return array.bio;
   }
 }
