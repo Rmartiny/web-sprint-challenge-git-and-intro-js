4e7d659cbc9d770b06c9018c0267b2cc5244364c
Rmartiny <dimambroraven@gmail.com>
2022-12-08 15:38:44 -0500

    task 8

--- index.js
+++ index.js
@@ -343,7 +343,7 @@ Use artistInfo to do the following:
 function artistInfo(array,string){
   let bioarray =[];
   for (let i=0 ;i < array.length; i++)
-if (array[i].includes(string));
+if (array[i].(string));
 {
 bioarray.push(artists[i].bio)
 
