3f158f7a6f1912239654384c6fa933474349db70
Rmartiny <dimambroraven@gmail.com>
2022-12-12 12:46:21 -0500

    task 8

--- index.js
+++ index.js
@@ -344,7 +344,7 @@ function artistInfo(array,name){
 
 for (let i = 0; i< array.length; i ++)
 if (array[i].name === name)
-return array[i].bio
+return array[i].bio}
 
   console.log (artistInfo(artists,'frida kahlo'))
 
